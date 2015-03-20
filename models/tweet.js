4dd331f6f3acc3255de9d3358b800d810ddf4476
Mário Silva <mjgs.biz@gmail.com>
2015-03-20 15:39:58 +0000

    Tweet Model import fix

--- models/tweet.js
+++ models/tweet.js
@@ -4,7 +4,7 @@ var TweetSchema = new mongoose.Schema({
       id: { type: String, required: true }
     , body: { type: String, required: true }
     , author: { type: String, required: true }
-    , name: { type: String, required: true }
+    , screenname: { type: String, required: true }
     , avatar: { type: String, required: true }
     , date : { type: Date, required: true }
     , active: { type: Boolean, required: true }
@@ -14,7 +14,7 @@ TweetSchema.statics.getTweets = function(page, skip, callback) {
     var tweets = [],
         start = (page * 10) + (skip * 1);
 
-    Tweet.find({},'id body author name avatar date active', { skip: start, limit: 10 }).sort({ date: 'desc' }).exec(function(err,docs){
+    this.find({},'id body author screenname avatar date active', { skip: start, limit: 10 }).sort({ date: 'desc' }).exec(function(err,docs){
        if(!err) {
            tweets = docs;
            tweets.forEach(function(tweet){
